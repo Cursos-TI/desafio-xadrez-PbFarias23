1e32674929f1ac5f2c6e59b7ee35ff788934843c
PbFarias23 <pbfc1906@gmail.com>
2025-10-29 13:22:31 +0000

    02

--- xadrez-mestre.c
+++ xadrez-mestre.c
@@ -6,11 +6,28 @@ int movimentoRainha = 8;
 int movimentoCavaloCima = 2;
 int movimentoCavaloDireita = 1;
 
+//recursiva torre
+
 void torre(int passo, int limite){
     if (passo>limite) return;
     printf("Direita\n");
     torre(passo + 1, limite);
 }
+
+//recursiva Rainha
+
+void rainha(int passo, int limite){
+    if (passo>limite) return;
+    printf("esquerda\n");
+    rainha(passo +1, limite);    
+}
+
+//recursiva Bispo
+
+
+
+
+
 
 int main(){
 
