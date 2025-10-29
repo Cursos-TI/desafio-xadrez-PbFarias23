6290bb899fb24744be6811e2da91fdda4a8b840a
PbFarias23 <pbfc1906@gmail.com>
2025-10-29 13:01:39 +0000

    nivel mestre01

--- xadrez-mestre.c
+++ xadrez-mestre.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int movimentoBispo = 5;
+int movimentoTorre = 5;
+int movimentoRainha = 8;
+int movimentoCavaloCima = 2;
+int movimentoCavaloDireita = 1;
+
+void torre(int passo, int limite){
+    if (passo>limite) return;
+    printf("Direita\n");
+    torre(passo + 1, limite);
+}
+
+int main(){
+
+
+    return 0;
+}
